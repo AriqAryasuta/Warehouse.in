3079b61b03e8571c3b4cfad533d8a8754dc77d04
rizkyintan <rizkyintan02@mail.ugm.ac.id>
2022-09-20 23:09:02 +0700

    menambahkan class admin

--- Warehouse.in/Warehouse.in/Admin.cs
+++ Warehouse.in/Warehouse.in/Admin.cs
@@ -9,17 +9,26 @@ namespace Warehouse.@in
     internal class Admin
     {
         private int _adminId;
+        
+        public int AdminId { get => _adminId; set => _adminId = value; }     
 
-        public int adminId
-        {
-            get { return _adminId; }
-            set { _adminId = value; }
-        }
         public void acceptRequest()
         {
 
         }
-        public void checkStock()
+        public bool checkStock()
+        {
+
+        }
+        public void Request()
+        {
+
+        }
+        public void sendItem()
+        {
+
+        }
+        public void updateStock()
         {
 
         }
