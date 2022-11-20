64a13b21cf6cf9723ee6724741dddd18474260a1
Rizky Intan Nurlita <79188089+rizkyintan@users.noreply.github.com>
2022-11-20 19:09:23 +0700

    Add current date time to homepage

--- Warehouse.in/Warehouse.in/formHome.Designer.cs
+++ Warehouse.in/Warehouse.in/formHome.Designer.cs
@@ -28,9 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dateNow = new System.Windows.Forms.Label();
+            this.timeNow = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,10 +43,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(38, 29);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(51, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(257, 26);
+            this.label1.Size = new System.Drawing.Size(324, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "Welcome back, Admin!";
             // 
@@ -51,35 +54,64 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(40, 55);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(53, 68);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(262, 17);
+            this.label2.Size = new System.Drawing.Size(311, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "Let’s start your inventory journey with us";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Warehouse.@in.Properties.Resources.g10;
-            this.pictureBox1.Location = new System.Drawing.Point(215, 143);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(287, 176);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(638, 430);
+            this.pictureBox1.Size = new System.Drawing.Size(851, 529);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // dateNow
+            // 
+            this.dateNow.AutoSize = true;
+            this.dateNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateNow.ForeColor = System.Drawing.Color.White;
+            this.dateNow.Location = new System.Drawing.Point(1002, 36);
+            this.dateNow.Name = "dateNow";
+            this.dateNow.Size = new System.Drawing.Size(67, 29);
+            this.dateNow.TabIndex = 6;
+            this.dateNow.Text = "Date";
+            // 
+            // timeNow
+            // 
+            this.timeNow.AutoSize = true;
+            this.timeNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeNow.ForeColor = System.Drawing.Color.White;
+            this.timeNow.Location = new System.Drawing.Point(1003, 65);
+            this.timeNow.Name = "timeNow";
+            this.timeNow.Size = new System.Drawing.Size(73, 20);
+            this.timeNow.TabIndex = 7;
+            this.timeNow.Text = "22:22:22";
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
             // formHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(60)))), ((int)(((byte)(187)))));
-            this.ClientSize = new System.Drawing.Size(1053, 642);
+            this.ClientSize = new System.Drawing.Size(1404, 790);
+            this.Controls.Add(this.timeNow);
+            this.Controls.Add(this.dateNow);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "formHome";
             this.Text = "formHome";
+            this.Load += new System.EventHandler(this.formHome_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,5 +123,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label dateNow;
+        private System.Windows.Forms.Label timeNow;
+        private System.Windows.Forms.Timer timer;
     }
 }
--- Warehouse.in/Warehouse.in/formHome.cs
+++ Warehouse.in/Warehouse.in/formHome.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
 
 namespace Warehouse.@in
@@ -15,6 +17,28 @@ namespace Warehouse.@in
         public formHome()
         {
             InitializeComponent();
+            //dateNow.Text = DateTime.Now.ToString("D");
+        }
+
+        private void formHome_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+    private void dateNow_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timeNow.Text = DateTime.Now.ToString("HH:mm:ss");
+            dateNow.Text = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+        }
+
+        private void timeNow_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
