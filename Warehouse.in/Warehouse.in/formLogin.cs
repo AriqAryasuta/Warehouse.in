7579e01d2a06b24e737b7c9e38f76bf71893a613
Rizky Intan Nurlita <79188089+rizkyintan@users.noreply.github.com>
2022-11-20 17:51:20 +0700

    Add sign up and login page

--- Warehouse.in/Warehouse.in/formLogin.cs
+++ Warehouse.in/Warehouse.in/formLogin.cs
@@ -29,5 +29,53 @@ namespace Warehouse.@in
             homepage.Show();
             this.Hide();
         }
+
+        private void tbUsername_Enter(object sender, EventArgs e)
+        {
+            if(tbUsername.Text == "Enter username")
+            {
+                tbUsername.Text = "";
+
+                tbUsername.StateCommon.Content.Color1 = Color.Black;
+            }
+        }
+
+        private void tbUsername_Leave(object sender, EventArgs e)
+        {
+            if (tbUsername.Text == "")
+            {
+                tbUsername.Text = "Enter username";
+
+                tbUsername.StateCommon.Content.Color1 = Color.DarkGray;
+            }
+        }
+
+        private void tbPassword_Enter(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == "Enter password")
+            {
+                tbPassword.Text = "";
+
+                tbPassword.StateCommon.Content.Color1 = Color.Black;
+            }
+        }
+
+        private void tbPassword_Leave(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == "")
+            {
+                tbPassword.Text = "Enter password";
+
+                tbPassword.StateCommon.Content.Color1 = Color.DarkGray;
+            }
+        }
+
+        private void lblSignup_Click(object sender, EventArgs e)
+        {
+            FormSignup signup = new FormSignup();
+
+            signup.Show();
+            this.Hide();
+        }
     }
 }
